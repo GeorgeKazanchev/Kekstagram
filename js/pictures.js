bbd58e57d378295ebf4f9d2984d4c066632a2744
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-12-02 20:40:25 +0300

    Проведён небольшой рефакторинг

--- js/pictures.js
+++ js/pictures.js
@@ -1,224 +1,200 @@
+'use strict';
+
 const PICTURES_COUNT = 25;
 const MIN_LIKES_COUNT = 15;
 const MAX_LIKES_COUNT = 200;
 const MAX_COMMENTS_COUNT = 2;
-const ESC_KEY_CODE = 27;
 
 const COMMENTS = [
-    `Всё отлично!`,
-    `В целом всё неплохо. Но не всё.`,
-    `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
-    `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
-    `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
-    `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
+  `Всё отлично!`,
+  `В целом всё неплохо. Но не всё.`,
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`,
 ];
 
 const DESCRIPTIONS = [
-    `Тестим новую камеру!`,
-    `Затусили с друзьями на море`,
-    `Как же круто тут кормят`,
-    `Отдыхаем...`,
-    `Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......`,
-    `Вот это тачка!`
+  `Тестим новую камеру!`,
+  `Затусили с друзьями на море`,
+  `Как же круто тут кормят`,
+  `Отдыхаем...`,
+  `Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......`,
+  `Вот это тачка!`,
 ];
 
-const commentTemplate =
-    `<li class="social__comment social__comment--text">
-        <img class="social__picture" src="" alt="Аватар комментатора фотографии" width="35" height="35">
-        <p class="social__text"></p>
-    </li>`;
-
 const pictureTemplate = document.querySelector('#picture');
+const commentTemplate = document.querySelector('#comment');
+const picturesContainerElement = document.querySelector('.pictures');
 const bigPictureElement = document.querySelector('.big-picture');
 const bigPictureCloseElement = document.querySelector('.big-picture__cancel');
 const formInputElement = document.querySelector('#upload-file');
 const imgUploadFormElement = document.querySelector('.img-upload__overlay');
 const imgUploadCloseElement = document.querySelector('.img-upload__cancel');
 
-const createElementFromTemplate = (template) => {
-    const templateContainer = document.createElement('div');
-    templateContainer.insertAdjacentHTML('afterbegin', template);
-    return templateContainer.firstElementChild;
+const getRandomArrayItem = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)];
 };
 
-const generatePicture = (index) => {
-    const url = `photos/${index + 1}.jpg`;
-    const likes = Math.floor(Math.random() * (MAX_LIKES_COUNT - MIN_LIKES_COUNT) + MIN_LIKES_COUNT);
-    const description = DESCRIPTIONS[Math.floor(Math.random() * DESCRIPTIONS.length)];
-
-    const commentsCount = Math.floor(Math.random() * MAX_COMMENTS_COUNT + 1);
-    const comments = [];
-    for (let j = 0; j < commentsCount; ++j) {
-        comments.push(COMMENTS[Math.floor(Math.random() * COMMENTS.length)]);
-    }
-
-    return {
-        url,
-        likes,
-        comments,
-        description
-    };
+const getRandomInRange = (min = 0, max = 0) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const generatePictures = () => {
-    const pictures = [];
-    for (let i = 0; i < PICTURES_COUNT; ++i) {
-        const picture = generatePicture(i);
-        pictures.push(picture);
-    }
-    return pictures;
-};
+  const pictures = [];
+  for (let i = 0; i < PICTURES_COUNT; ++i) {
+    const url = `photos/${i + 1}.jpg`;
+    const likes = Math.floor(
+      Math.random() * (MAX_LIKES_COUNT - MIN_LIKES_COUNT) + MIN_LIKES_COUNT
+    );
+    const description = getRandomArrayItem(DESCRIPTIONS);
+    const commentsCount = getRandomInRange(1, 2);
+    const comments = new Array(commentsCount).fill(getRandomArrayItem(COMMENTS));
 
-const createPictureElement = (picture) => {
-    const pictureElement = pictureTemplate.content.cloneNode(true);
-    const imageElement = pictureElement.querySelector('.picture__img');
-    const likesCountElement = pictureElement.querySelector('.picture__likes');
-    const commentsCountElement = pictureElement.querySelector('.picture__comments');
-    imageElement.src = picture.url;
-    likesCountElement.textContent = picture.likes;
-    commentsCountElement.textContent = picture.comments.length;
-    return pictureElement;
-};
-
-const createPictureElements = (pictures) => {
-    return pictures.map((picture) => createPictureElement(picture));
-};
-
-const createCommentElement = (comment) => {
-    const commentElement = createElementFromTemplate(commentTemplate);
-    const imageElement = commentElement.querySelector('.social__picture');
-    const textElement = commentElement.querySelector('.social__text');
-    const imageNumber = Math.floor(Math.random() * 5 + 1);
-    imageElement.src = `img/avatar-${imageNumber}.svg`;
-    textElement.textContent = comment;
-    return commentElement;
-};
-
-const createCommentElements = (comments) => {
-    return comments.map((comment) => createCommentElement(comment));
-};
-
-const showPictureElements = (pictureElements) => {
-    const picturesContainerElement = document.querySelector('.pictures');
-    const fragment = new DocumentFragment();
-    fragment.append(...pictureElements);
-    picturesContainerElement.append(fragment);
-    picturesContainerElement.classList.remove('visually-hidden');
-};
-
-const renderCommentsList = (commentElements) => {
-    const listElement = document.querySelector('.social__comments');
-    listElement.innerHTML = '';
-    const fragment = new DocumentFragment();
-    fragment.append(...commentElements);
-    listElement.append(fragment);
-};
-
-const showBigPicture = (picture) => {
-    bigPictureElement.classList.remove('hidden');
-    setTimeout(() => bigPictureCloseElement.focus(), 0);    //  It's added for providing a way of closing picture by Esc
-
-    const imageElement = bigPictureElement.querySelector('.big-picture__img > img');
-    const likesCountElement = bigPictureElement.querySelector('.likes-count');
-    const commentsCountElement = bigPictureElement.querySelector('.comments-count');
-    const descriptionElement = bigPictureElement.querySelector('.social__caption');
-
-    imageElement.src = picture.url;
-    likesCountElement.textContent = picture.likes;
-    commentsCountElement.textContent = picture.comments.length;
-    descriptionElement.textContent = picture.description;
-
-    const commentElements = createCommentElements(picture.comments);
-    renderCommentsList(commentElements);
-};
-
-const hideBigPicture = () => {
-    bigPictureElement.classList.add('hidden');  
-};
-
-const hideBigPictureComments = () => {
-    const commentsCountElement = bigPictureElement.querySelector('.social__comment-count');
-    const commentsLoaderElement = bigPictureElement.querySelector('.social__comments-loader');
-    commentsCountElement.classList.add('visually-hidden');
-    commentsLoaderElement.classList.add('visually-hidden');
-};
-
-const addUploadFileFormChangeHandler = () => {
-    formInputElement.addEventListener('change', () => {
-        imgUploadFormElement.classList.remove('hidden');
-        setTimeout(() => imgUploadCloseElement.focus(), 0);
+    pictures.push({
+      url,
+      likes,
+      comments,
+      description,
     });
+  }
+  return pictures;
 };
 
-const addUploadFileFormCloseClickHandler = () => {
-    imgUploadCloseElement.addEventListener('click', (evt) => {
-        evt.preventDefault();
-        closeUploadFileForm();
-    });
-};
+const renderPicture = (picture) => {
+  const pictureElement = pictureTemplate.content.cloneNode(true);
 
-const addUploadFileFormCloseKeyDownHandler = () => {
-    imgUploadFormElement.addEventListener('keydown', (evt) => {
-        if (evt.keyCode === ESC_KEY_CODE) {
-            closeUploadFileForm();
-        }
-    });
-};
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
 
-const closeUploadFileForm = () => {
-    const imgUploadFormElement = document.querySelector('.img-upload__overlay');
-    imgUploadFormElement.classList.add('hidden');
-    formInputElement.value = '';
-};
-
-const addPicturesClickHandlers = (pictures) => {    //  TODO: Maybe, It'll be renamed
-    const pictureElements = document.querySelectorAll('.picture');
-    pictureElements.forEach((pictureElement) => {
-        pictureElement.addEventListener('click', (evt) => {
-            evt.preventDefault();
-            const picture = getPictureByElement(pictureElement, pictures);
-            showBigPicture(picture);
-            hideBigPictureComments();
-        });
-    });
-};
-
-const getPictureFilename = (url) => {
-    return url.split('/').at(-1);
-};
-
-const getPictureByElement = (pictureElement, pictures) => {
-    const givenPictureFilename = getPictureFilename(pictureElement.querySelector('.picture__img').src);
-    const foundPicture = pictures.find((picture) => {
-        const curPictureFilename = getPictureFilename(picture.url);
-        return curPictureFilename === givenPictureFilename;
-    });
-
-    if (foundPicture === undefined) {
-        throw new Error('Failed to find an appropriate picture for element.');
-    }
-
-    return foundPicture;
-};
-
-const addBigPictureCloseClickHandler = () => {
-    bigPictureCloseElement.addEventListener('click', (evt) => {
-        evt.preventDefault();
-        hideBigPicture();
-    });
-};
-
-const addBigPictureCloseKeyDownHandler = () => {
-    bigPictureElement.addEventListener('keydown', (evt) => {
-        if (evt.keyCode === ESC_KEY_CODE) {
-            hideBigPicture();
-        }
-    });
+  return pictureElement;
 };
 
 const pictures = generatePictures();
-const pictureElements = createPictureElements(pictures);
-showPictureElements(pictureElements);
+const fragment = document.createDocumentFragment();
+pictures.forEach((picture) => {
+  fragment.append(renderPicture(picture));
+});
+
+picturesContainerElement.append(fragment);
+
+const renderComment = (comment) => {
+  const commentElement = commentTemplate.content.cloneNode(true);
+
+  const imageNumber = getRandomInRange(1, 6);
+  commentElement.querySelector('.social__picture').src = `img/avatar-${imageNumber}.svg`;
+  commentElement.querySelector('.social__text').textContent = comment;
+
+  return commentElement;
+};
+
+const renderComments = (comments, renderTo) => {
+  const fragment = document.createDocumentFragment();
+  comments.forEach((comment) => {
+    fragment.append(renderComment(comment));
+  });
+  renderTo.append(fragment);
+};
+
+const showBigPicture = (picture) => {
+  bigPictureElement.classList.remove('hidden');
+  setTimeout(() => bigPictureCloseElement.focus(), 0); //  It's added for providing a way of closing picture by Esc
+
+  bigPictureElement.querySelector('.big-picture__img > img').src = picture.url;
+  bigPictureElement.querySelector('.likes-count').textContent = picture.likes;
+  bigPictureElement.querySelector('.comments-count').textContent = picture.comments.length;
+  bigPictureElement.querySelector('.social__caption').textContent = picture.description;
+
+  const commentsContainer = bigPictureElement.querySelector('.social__comments');
+  commentsContainer.innerHTML = '';
+  renderComments(picture.comments, commentsContainer);
+};
+
+const hideBigPicture = () => {
+  bigPictureElement.classList.add('hidden');
+};
+
+const hideBigPictureComments = () => {
+  bigPictureElement.querySelector('.social__comment-count').classList.add('visually-hidden');
+  bigPictureElement.querySelector('.social__comments-loader').classList.add('visually-hidden');
+};
+
+const addUploadFileFormChangeHandler = () => {
+  formInputElement.addEventListener('change', () => {
+    imgUploadFormElement.classList.remove('hidden');
+    setTimeout(() => imgUploadCloseElement.focus(), 0);
+  });
+};
+
+const addUploadFileFormCloseClickHandler = () => {
+  imgUploadCloseElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    closeUploadFileForm();
+  });
+};
+
+const addUploadFileFormCloseKeyDownHandler = () => {
+  imgUploadFormElement.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      closeUploadFileForm();
+    }
+  });
+};
+
+const closeUploadFileForm = () => {
+  const imgUploadFormElement = document.querySelector('.img-upload__overlay');
+  imgUploadFormElement.classList.add('hidden');
+  formInputElement.value = '';
+};
+
+const addPicturesClickHandlers = (pictures) => {
+  //  TODO: Maybe, It'll be renamed
+  const pictureElements = document.querySelectorAll('.picture');
+  pictureElements.forEach((pictureElement) => {
+    pictureElement.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      const picture = getPictureByElement(pictureElement, pictures);
+      showBigPicture(picture);
+      hideBigPictureComments();
+    });
+  });
+};
+
+const getPictureFilename = (url) => {
+  return url.split('/').at(-1);
+};
+
+const getPictureByElement = (pictureElement, pictures) => {
+  const givenPictureFilename = getPictureFilename(
+    pictureElement.querySelector('.picture__img').src
+  );
+  const foundPicture = pictures.find((picture) => {
+    const curPictureFilename = getPictureFilename(picture.url);
+    return curPictureFilename === givenPictureFilename;
+  });
+
+  if (foundPicture === undefined) {
+    throw new Error('Failed to find an appropriate picture for element.');
+  }
+
+  return foundPicture;
+};
+
+const addBigPictureCloseClickHandler = () => {
+  bigPictureCloseElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    hideBigPicture();
+  });
+};
+
+const addBigPictureCloseKeyDownHandler = () => {
+  bigPictureElement.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
+      hideBigPicture();
+    }
+  });
+};
 
 addUploadFileFormChangeHandler();
 addUploadFileFormCloseClickHandler();
